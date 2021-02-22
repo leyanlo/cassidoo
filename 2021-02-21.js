63f07c14222da1f5af603c98f5ddd62561a28984
Leyan Lo <leyanlo@gmail.com>
2021-02-22 00:18:35 -0800

    feat: 2021-02-21

---
+++ 2021-02-21.js
@@ -0,0 +1,13 @@
+function everyOther(str) {
+  let nDelete = 0;
+  let prevChar = str[0];
+  for (let i = 1; i < str.length; ++i) {
+    const char = str[i];
+    nDelete += char === prevChar;
+    prevChar = char;
+  }
+  return nDelete;
+}
+
+console.log(everyOther("xxyxxy"));
+console.log(everyOther("yyyyy"));
