15e45f150f7f0e6153d00d412edc673d77cb7daa
Leyan Lo <leyanlo@gmail.com>
2021-06-21 20:22:21 -0700

    chore(2021-06-21): consolidate helpers to drawLine() and add odd test case

--- 2021-06-21.test.js
+++ 2021-06-21.test.js
@@ -1,46 +1,58 @@
-function addVertices(lines, x) {
-  lines[~~(x / 2) - 1][~~(x / 2) + 1] = '+';
-  lines[~~(x / 2) - 1][~~((5 / 2) * x) + 2] = '+';
-  lines[x][0] = '+';
-  lines[x][2 * x + 1] = '+';
-  lines[~~((3 / 2) * x)][~~((5 / 2) * x) + 2] = '+';
-  lines[2 * x + 1][0] = '+';
-  lines[2 * x + 1][2 * x + 1] = '+';
-}
-
-function addHorizontals(lines, x) {
-  for (let i = 0; i < 2 * x; i++) {
-    lines[x / 2 - 1][x / 2 + 2 + i] = '-';
-    lines[x][1 + i] = '-';
-    lines[2 * x + 1][i + 1] = '-';
-  }
-}
-
-function addVerticals(lines, x) {
-  for (let i = 0; i < x; i++) {
-    lines[~~(x / 2) + i][~~((5 / 2) * x) + 2] = '|';
-    lines[x + 1 + i][0] = '|';
-    lines[x + 1 + i][2 * x + 1] = '|';
-  }
-}
-
-function addDiagonals(lines, x) {
-  for (let i = 0; i < ~~(x / 2); i++) {
-    lines[~~(x / 2) + i][~~(x / 2) - i] = '/';
-    lines[~~(x / 2) + i][~~((5 / 2) * x) + 1 - i] = '/';
-    lines[~~((3 / 2) * x + 1 + i)][~~((5 / 2) * x) + 1 - i] = '/';
+// draw a line of chars connecting [r1, c1] to [r2, c2], excluding the endpoints
+function drawLine(img, [r1, c1], [r2, c2], char) {
+  const dr = r2 === r1 ? 0 : r2 > r1 ? 1 : -1;
+  const dc = c2 === c1 ? 0 : c2 > c1 ? 1 : -1;
+  for (
+    let r = r1 + dr, c = c1 + dc;
+    (dr !== 0 && (dr > 0 ? r < r2 : r > r2)) ||
+    (dc !== 0 && (dc > 0 ? c < c2 : c > c2));
+    r += dr, c += dc
+  ) {
+    img[r][c] = char;
   }
 }
 
 function drawCube(x) {
-  const lines = [...Array(2 * x + 2)].map(() =>
+  const img = [...Array(2 * x + 2)].map(() =>
     [...Array(~~(2.5 * x) + 3)].map(() => ' ')
   );
-  addVertices(lines, x);
-  addHorizontals(lines, x);
-  addVerticals(lines, x);
-  addDiagonals(lines, x);
-  console.log(lines.map((line) => line.join('').trimEnd()).join('\n'));
+
+  /**
+   * vertices are indexed as follows:
+   *   0----1
+   *  /    /|
+   * 2----3 |
+   * |    | 4
+   * |    |/
+   * 5----6
+   */
+  const v = [
+    [~~(x / 2) - 1, ~~(x / 2) + 1],
+    [~~(x / 2) - 1, ~~(2.5 * x) + 2],
+    [x, 0],
+    [x, 2 * x + 1],
+    [~~(1.5 * x), ~~(2.5 * x) + 2],
+    [2 * x + 1, 0],
+    [2 * x + 1, 2 * x + 1],
+  ];
+
+  for (const [r, c] of v) {
+    img[r][c] = '+';
+  }
+
+  drawLine(img, v[0], v[1], '-');
+  drawLine(img, v[2], v[3], '-');
+  drawLine(img, v[5], v[6], '-');
+
+  drawLine(img, v[0], v[2], '/');
+  drawLine(img, v[1], v[3], '/');
+  drawLine(img, v[4], v[6], '/');
+
+  drawLine(img, v[1], v[4], '|');
+  drawLine(img, v[2], v[5], '|');
+  drawLine(img, v[3], v[6], '|');
+
+  console.log(img.map((row) => row.join('').trimEnd()).join('\n'));
 }
 
 test('drawCube', () => {
@@ -65,6 +77,19 @@ test('drawCube', () => {
 |        | /
 |        |/
 +--------+`);
+  drawCube(5);
+  expect(console.log).toHaveBeenLastCalledWith(`
+   +----------+
+  /          /|
+ /          / |
+/          /  |
++----------+  |
+|          |  |
+|          |  +
+|          | /
+|          |/
+|          |
++----------+`);
   drawCube(6);
   expect(console.log).toHaveBeenLastCalledWith(`
 
