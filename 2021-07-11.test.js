09eb4f0a4448db5b43852e73c9f52bb55b19391b
Leyan Lo <leyan@uber.com>
2021-07-11 23:38:58 -0700

    feat: 2021-07-11

---
+++ 2021-07-11.test.js
@@ -0,0 +1,18 @@
+function ipv4ToBinary(addr) {
+  return addr
+    .split('.')
+    .map((octet) => parseInt(octet, 10).toString(2).padStart(8, '0'))
+    .join('');
+}
+
+function inRange(addr, netmask) {
+  const binAddr = ipv4ToBinary(addr);
+  const [maskAddr, maskBits] = netmask.split('/');
+  const binMaskAddr = ipv4ToBinary(maskAddr);
+  return binAddr.substring(0, maskBits) === binMaskAddr.substring(0, maskBits);
+}
+
+test('inRange', () => {
+  expect(inRange('192.168.4.27', '192.168.0.0/16')).toBe(true);
+  expect(inRange('192.168.4.27', '192.168.1.0/24')).toBe(false);
+});
