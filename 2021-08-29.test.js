c81e2fea3b2730d462615e11830f0466fd36599f
Leyan Lo <leyanlo@gmail.com>
2021-08-29 21:40:06 -0700

    feat: 2021-08-29

---
+++ 2021-08-29.test.js
@@ -0,0 +1,18 @@
+function longestPrefix(arr) {
+  let prefix = arr[0] ?? ''
+  for (let i = 1; i < arr.length && prefix; i++) {
+    const str = arr[i];
+    for (let j = 0; j < prefix.length; j++) {
+      if (prefix[j] !== str[j]) {
+        prefix = prefix.substring(0, j);
+        break;
+      }
+    }
+  }
+  return prefix;
+}
+
+test('longestPrefix', () => {
+  expect(longestPrefix(['cranberry', 'crawfish', 'crap'])).toBe('cra');
+  expect(longestPrefix(['parrot', 'poodle', 'fish'])).toBe('');
+});
