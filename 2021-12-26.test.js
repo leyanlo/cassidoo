fb308b6ae13a1ba83cb1d77af5556cdeda754781
Leyan Lo <leyanlo@gmail.com>
2021-12-29 17:17:42 -0800

    fix(2021-12-12): fix solution

--- 2021-12-26.test.js
+++ 2021-12-26.test.js
@@ -5,36 +5,45 @@ function orderFireworks(fireworks) {
     counts[firework] = (counts[firework] ?? 0) + 1;
   }
 
-  // map count to fireworks
-  const map = [];
-  for (const [firework, count] of Object.entries(counts)) {
-    map[count] = map[count] ?? [];
-    map[count].push(firework);
-  }
+  // sort from most to least frequent
+  const order = Object.entries(counts)
+    .sort(([, c1], [, c2]) => c2 - c1)
+    .flatMap(([firework, count]) => Array(count).fill(firework));
 
-  // build firing order based on map
-  const order = [];
-  for (let i = map.length - 1; i > 0; i--) {
-    order.push(...map[i]);
-    map[i - 1] = map[i - 1] ?? [];
-    map[i - 1].push(...map[i]);
-  }
-
-  // return null for firing orders containing duplicates
-  for (let i = 0; i < order.length - 2; i++) {
-    if (order[i] === order[i + 1]) {
+  // swap until order is valid
+  outer: for (let i = 1; i < order.length; i++) {
+    if (order[i] === order[i - 1]) {
+      for (let j = i + 1; j < order.length; j++) {
+        if (order[j] !== order[i]) {
+          order[i] = order[j];
+          order[j] = order[i - 1];
+          continue outer;
+        }
+      }
+      // valid swap not found
       return null;
     }
   }
-
   return order;
 }
 
 test('orderFireworks', () => {
   expect(
     orderFireworks(['green', 'green', 'green', 'red', 'red', 'blue'])
-  ).toEqual(['green', 'red', 'green', 'blue', 'red', 'green']);
+  ).toEqual(['green', 'red', 'green', 'red', 'green', 'blue']);
   expect(
     orderFireworks(['green', 'green', 'green', 'green', 'red', 'red', 'blue'])
+  ).toEqual(['green', 'red', 'green', 'red', 'green', 'blue', 'green']);
+  expect(
+    orderFireworks([
+      'green',
+      'green',
+      'green',
+      'green',
+      'green',
+      'red',
+      'red',
+      'blue',
+    ])
   ).toBe(null);
 });
