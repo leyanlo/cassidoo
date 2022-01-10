74e436b4f382e618418e0c38d1dcbd265bfdfe7f
Leyan Lo <leyanlo@gmail.com>
2022-01-09 23:31:08 -0800

    feat: 2022-01-09

---
+++ 2022-01-09.test.js
@@ -0,0 +1,17 @@
+function capPermutations(s) {
+  let permutations = new Set(['']);
+  for (const char of s) {
+    const nextPermutations = new Set();
+    for (const p of permutations) {
+      nextPermutations.add(p + char.toLowerCase())
+      nextPermutations.add(p + char.toUpperCase())
+    }
+    permutations = nextPermutations;
+  }
+  return [...permutations]
+}
+
+test('capPermutations', () => {
+  expect(capPermutations('ab2')).toEqual(['ab2', 'aB2', 'Ab2', 'AB2']);
+  expect(capPermutations('35p')).toEqual(['35p', '35P']);
+});
