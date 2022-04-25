ed537c7e97c5fbe8ab2426c95531e4ee58ce0f58
Leyan Lo <leyanlo@gmail.com>
2022-04-24 22:34:06 -0700

    feat: 2022-04-24

---
+++ src/2022/2022-04-24.test.js
@@ -0,0 +1,35 @@
+function mergeIntervals(intervals) {
+  return intervals
+    .sort(([a], [b]) => a - b)
+    .reduce((acc, curr) => {
+      const prev = acc[acc.length - 1];
+      if (prev && curr[0] <= prev[1]) {
+        prev[1] = Math.max(prev[1], curr[1]);
+      } else {
+        acc.push(curr);
+      }
+      return acc;
+    }, []);
+}
+
+test('mergeIntervals', () => {
+  expect(
+    mergeIntervals([
+      [1, 4],
+      [2, 6],
+      [8, 10],
+      [15, 20],
+    ])
+  ).toEqual([
+    [1, 6],
+    [8, 10],
+    [15, 20],
+  ]);
+
+  expect(
+    mergeIntervals([
+      [1, 2],
+      [2, 7],
+    ])
+  ).toEqual([[1, 7]]);
+});
