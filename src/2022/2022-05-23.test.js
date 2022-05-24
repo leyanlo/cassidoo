aa875f1c2289c88e8295df25cbb10a044e71a117
Leyan Lo <leyanlo@gmail.com>
2022-05-23 22:43:54 -0700

    feat: 2022-05-23

---
+++ src/2022/2022-05-23.test.js
@@ -0,0 +1,84 @@
+const dirs = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
+
+const dirName = {
+  '0,1': 'right',
+  '1,0': 'down',
+  '0,-1': 'left',
+  '-1,0': 'up',
+};
+
+function startToEnd(mat) {
+  let paths;
+  outer: for (let i = 0; i < mat.length; i++) {
+    for (let j = 0; j < mat[i].length; j++) {
+      if (mat[i][j] === 1) {
+        paths = [[[i, j]]];
+        break outer;
+      }
+    }
+  }
+  while (true) {
+    const nextPaths = [];
+    let foundEnd = false;
+    for (const path of paths) {
+      const [i, j] = path[path.length - 1];
+      for (const [di, dj] of dirs) {
+        const i2 = i + di;
+        const j2 = j + dj;
+        switch (mat[i2]?.[j2]) {
+          case 2:
+            foundEnd = true;
+          // intentionally fall through
+          case 0:
+            nextPaths.push([...path, [i2, j2]]);
+        }
+      }
+    }
+    paths = nextPaths;
+    if (foundEnd) {
+      break;
+    }
+    // avoid retracing by adding walls
+    for (const path of paths) {
+      const [i, j] = path[path.length - 1];
+      mat[i][j] = 3;
+    }
+  }
+  return paths
+    .filter((path) => {
+      const [i, j] = path[path.length - 1];
+      return mat[i][j] === 2;
+    })
+    .map((path) =>
+      path.slice(1).map(([i2, j2], idx) => {
+        const [i, j] = path[idx];
+        const dir = [i2 - i, j2 - j];
+        return dirName[dir.join()];
+      })
+    );
+}
+
+test('startToEnd', () => {
+  let grid = [
+    [1, 0, 0],
+    [0, 0, 2],
+  ];
+
+  let grid2 = [
+    [1, 3, 0],
+    [0, 0, 2],
+  ];
+
+  expect(startToEnd(grid)).toEqual([
+    ['right', 'right', 'down'],
+    ['right', 'down', 'right'],
+    ['down', 'right', 'right'],
+  ]);
+
+  expect(startToEnd(grid2)).toEqual([['down', 'right', 'right']]);
+});
