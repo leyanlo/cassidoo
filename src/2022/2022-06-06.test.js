99ff6a02907bcf003a2bab37fc929c0263a218ad
Leyan Lo <leyanlo@gmail.com>
2022-06-05 23:53:17 -0700

    feat: 2022-06-06

---
+++ src/2022/2022-06-06.test.js
@@ -0,0 +1,35 @@
+function coinComboHelper(coins, amount, memo) {
+  // assume coins sorted ascending
+  if (amount < coins[0]) {
+    return [];
+  }
+  // check from largest to smallest coin
+  for (let i = coins.length - 1; i >= 0; i--) {
+    const coin = coins[i];
+    if (amount === coin) {
+      return [coin];
+    }
+
+    const nextAmount = amount - coin;
+    memo[nextAmount] =
+      memo[nextAmount] ?? coinComboHelper(coins, nextAmount, memo);
+    const nextCombo = memo[nextAmount];
+    if (nextCombo.length) {
+      return [...nextCombo, coin];
+    }
+  }
+  return [];
+}
+
+function coinCombo(coins, amount) {
+  return coinComboHelper(
+    coins.sort((a, b) => a - b),
+    amount,
+    {}
+  );
+}
+
+test('coinCombo', () => {
+  expect(coinCombo([2, 3, 5, 7], 17)).toEqual([3, 7, 7]);
+  expect(coinCombo([2, 3, 5], 11)).toEqual([3, 3, 5]);
+});
