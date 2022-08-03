34b80b6d9e81fcd572d134b433cac1ef2c344469
Leyan Lo <leyanlo@gmail.com>
2022-08-02 18:56:15 -0700

    feat: 2022-07-31

---
+++ src/2022/2022-07-31.test.js
@@ -0,0 +1,34 @@
+// O(n) brute force solution
+function numberOfOnesSlow(n) {
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    count += i
+      .toString(10)
+      .split('')
+      .map((d) => d === '1')
+      .reduce((acc, b) => acc + b);
+  }
+  return count;
+}
+
+// O(log(n)) solution by calculating series:
+//   ~~((n + 9) / 10) +
+//     Math.max(~~((n + 81) / 100) * 10, n - 9 - ~~((n + 81) / 100) * 90) +
+//     Math.max(~~((n + 801) / 1000) * 100, n - 99 - ~~((n + 801) / 1000) * 900) + ...
+function numberOfOnes(n) {
+  let ans = 0;
+  for (let pow = 1; pow <= n; pow *= 10) {
+    const step = ~~((n + 8 * pow + 1) / (10 * pow));
+    ans += Math.max(step * pow, n - (pow - 1) - step * 9 * pow);
+  }
+  return ans;
+}
+
+test('numberOfOnes', () => {
+  expect(numberOfOnes(14)).toBe(7);
+
+  // verify against slow solution
+  for (let n = 1; n <= 1000; n++) {
+    expect(numberOfOnes(n)).toBe(numberOfOnesSlow(n));
+  }
+});
