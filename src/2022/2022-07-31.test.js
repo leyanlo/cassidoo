45efe61ebff8c73037e84309486d1bb16383df35
Leyan Lo <leyanlo@gmail.com>
2022-08-19 00:41:58 -0700

    chore(2022-07-31): simplify logic
    Write out piecewise function explicitly rather than combining sawtooth and step functions.

--- src/2022/2022-07-31.test.js
+++ src/2022/2022-07-31.test.js
@@ -12,15 +12,15 @@ function numberOfOnesSlow(n) {
 }
 
 // O(log(n)) solution by calculating series:
-//   ~~((n + 9) / 10) +
-//     Math.max(~~((n + 81) / 100) * 10, n - 9 - ~~((n + 81) / 100) * 90) +
-//     Math.max(~~((n + 801) / 1000) * 100, n - 99 - ~~((n + 801) / 1000) * 900) + ...
+//   ~~(n / 10) + (n % 10 < 1 ? 0 : 1) +
+//   ~~(n / 100) * 10 + (n % 100 < 10 ? 0 : n % 100 < 20 ? (n % 100) - 9 : 10) +
+//   ~~(n / 1000) * 100 + (n % 1000 < 100 ? 0 : n % 1000 < 200 ? (n % 1000) - 99 : 100) + ...
 function numberOfOnes(n) {
   let count = 0;
   for (let pow = 1; pow <= n; pow *= 10) {
-    const stepFn = ~~((n + 8 * pow + 1) / (10 * pow)) * pow;
-    const sawtoothFn = n - (pow - 1) - stepFn * 9;
-    count += Math.max(stepFn, sawtoothFn);
+    const q = ~~(n / (10 * pow));
+    const r = n % (10 * pow);
+    count += q * pow + (r < pow ? 0 : r < 2 * pow ? r - pow + 1 : pow);
   }
   return count;
 }
