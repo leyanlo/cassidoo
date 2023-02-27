ebe25d87a0f35d24dab0304243f20ad700f0daeb
Leyan Lo <leyanlo@gmail.com>
2023-02-27 10:28:52 -0800

    feat(2023-02-26): solve

---
+++ src/2023/2023-02-26.test.js
@@ -0,0 +1,22 @@
+function repeatedGroups(nums) {
+  const groups = [];
+  let currGroup;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      currGroup = [];
+      groups.push(currGroup);
+    }
+    currGroup.push(nums[i]);
+  }
+  return groups.filter((group) => group.length > 1);
+}
+
+test('repeatedGroups', () => {
+  expect(repeatedGroups([1, 2, 2, 4, 5])).toEqual([[2, 2]]);
+  expect(repeatedGroups([1, 1, 0, 0, 8, 4, 4, 4, 3, 2, 1, 9, 9])).toEqual([
+    [1, 1],
+    [0, 0],
+    [4, 4, 4],
+    [9, 9],
+  ]);
+});
