2d5b767665760c660019d96560c74e79a923b917
Leyan Lo <leyanlo@gmail.com>
2023-07-10 14:23:44 -0700

    feat(2023-07-09): solve

---
+++ src/2023/2023-07-09.test.js
@@ -0,0 +1,36 @@
+function justifyText(arr, width) {
+  const lines = [];
+  let lineLength = 0;
+  let start = 0;
+  let end = 0;
+  while (end <= arr.length) {
+    if (
+      end !== arr.length &&
+      lineLength + end - start + arr[end].length <= width
+    ) {
+      lineLength += arr[end].length;
+    } else {
+      let line = '';
+      let lineSpaces = width - lineLength;
+      for (let i = start; i < end; i++) {
+        const chunkSpaces = Math.ceil(lineSpaces / (end - i - 1 || 1));
+        line += arr[i].padEnd(chunkSpaces + arr[i].length);
+        lineSpaces -= chunkSpaces;
+      }
+      lines.push(line);
+      lineLength = arr[end]?.length ?? 0;
+      start = end;
+    }
+    end++;
+  }
+  return lines;
+}
+
+test('justifyText', () => {
+  // prettier-ignore
+  expect(justifyText(["This", "is", "an", "example", "of", "text", "justification."], 16)).toEqual([
+    "This    is    an",
+    "example  of text",
+    "justification.  "
+  ])
+});
