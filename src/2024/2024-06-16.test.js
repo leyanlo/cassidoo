048e3252f8c4232184540813c5f84cec8af7a06d
Leyan Lo <leyanlo@gmail.com>
2024-06-17 12:48:01 -0700

    feat(2024-06-16): solve

---
+++ src/2024/2024-06-16.test.js
@@ -0,0 +1,24 @@
+function nVowels(str) {
+  return str.match(/[aeiou]/gi).length;
+}
+
+function sortNames(names) {
+  return names.toSorted(
+    (a, b) => nVowels(b) - nVowels(a) || a.localeCompare(b)
+  );
+}
+
+test('sortNames', () => {
+  expect(sortNames(['Goku', 'Vegeta', 'Piccolo', 'Gohan'])).toStrictEqual([
+    'Piccolo',
+    'Vegeta',
+    'Gohan',
+    'Goku',
+  ]);
+  expect(sortNames(['Edward', 'Alphonse', 'Roy', 'Winry'])).toStrictEqual([
+    'Alphonse',
+    'Edward',
+    'Roy',
+    'Winry',
+  ]);
+});
