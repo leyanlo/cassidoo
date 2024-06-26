f2dbbd021e6e950c22e6a24076b82dea9289988b
Leyan Lo <leyanlo@gmail.com>
2024-06-26 00:10:40 -0700

    feat(2024-06-23): solve

---
+++ src/2024/2024-06-23.test.js
@@ -0,0 +1,22 @@
+function dailyTemperatures(temps) {
+  const stack = [];
+  const res = Array.from({ length: temps.length }).fill(0);
+
+  for (let i = 0; i < temps.length; i++) {
+    const temp = temps[i];
+    while (stack.at(-1)?.temp < temp) {
+      const { i: i2 } = stack.pop();
+      res[i2] = i - i2;
+    }
+    stack.push({ temp, i });
+  }
+  return res;
+}
+
+test('dailyTemperatures', () => {
+  expect(dailyTemperatures([70, 70, 70, 75])).toStrictEqual([3, 2, 1, 0]);
+  expect(dailyTemperatures([90, 80, 70, 60])).toStrictEqual([0, 0, 0, 0]);
+  expect(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])).toStrictEqual([
+    1, 1, 4, 2, 1, 1, 0, 0,
+  ]);
+});
